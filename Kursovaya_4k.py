476ec81dfecbeb3a3875ebee92421efdd116a3a3
SuperFeeder <64524141+maximim10@users.noreply.github.com>
2022-04-25 13:22:36 +0300

    Add files via upload

--- Kursovaya_4k.py
+++ Kursovaya_4k.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 import time
 from multiprocessing import Pool
 
-L=1; n=3; h=L/n; b=h*1000; gamma=1/2; E1=2; E2=E1/10; points=100000; scale=1
+L=1; n=20; h=L/n; b=h/1000; gamma=1/2; E1=2; E2=E1/10; points=100000; scale=n/3
 h0 = 1/(gamma/(E1)+(1-gamma)/(E2)); c2 = 1/2-h0*(gamma**2/E1/2+(1-gamma)*gamma/E1+(1-gamma)**2/E2/2)
 k1=0
+def Find_force(t):
+    return 2*b**2/((t-L/2)**2+b**2)
 def Find_G_part(t):
     G=b**2*math.log(4*b**2+(L-2*t)**2)-b*(L-2*t)*math.atan((L-2*t)/(2*b))
     return G
@@ -93,44 +95,49 @@ def N2(t):
         return A/2*t**2+B*t-c3
     else:
         return A/2*gamma**2+B*gamma-c3+C/2*t**2+D*t-c3-C/2*gamma**2-D*gamma
+
+def Find_stress(t):
+    pass
 if __name__ == '__main__':
     pool = Pool()
-    
     start_time = time.time()
-    
-    
     Find_k1()
     print (f"k1 = {k1}")
     fig = plt.subplots()
+    plt.subplot(2, 1, 1)
     x_local = [0.5-1/2/scale+i/points/scale for i in range(0,points+1)]
+    x_global = [i/points for i in range(0,points+1)]
     #y = [displacement_exact(x_local[i]) for i in range (0,points+1)]
     #d1 = [displacement_deg_1(x_local[i]) for i in range (0,points+1)]
-    #d2 = [displacement_deg_2(x_local[i]) for i in range (0,points+1)]
+    d2 = [displacement_deg_2(x_local[i]) for i in range (0,points+1)]
     print("th start")
     t_th_start=time.time()
     d_e_th = list(pool.starmap(displacement_exact_fast, zip(x_local, [k1 for i in range(len(x_local))])))
     pool.close()
     pool.join()
     pool = Pool()
-    d1_th = list(map(displacement_deg_1, x_local))
+    d1_th = list(pool.map(displacement_deg_1, x_local))
     pool.close()
     pool.join()
     pool = Pool()
-    d2_th = list(map(displacement_deg_2, x_local))
+    d2_th = list(pool.map(displacement_deg_2, x_local))
     pool.close()
     pool.join()
     print("th time  --- %s seconds ---" % (time.time() - t_th_start))
     #plt.plot(x_local, y, "y")
     plt.plot(x_local, d_e_th, "y")
     plt.plot(x_local, d1_th, "b--")
-    plt.plot(x_local, d2_th, "r--") 
+    plt.plot(x_local, d2_th, "r--")
+    plt.subplot(2, 1, 2)
+    Force_arr = list(map(Find_force, x_global))
+    plt.plot(x_global, Force_arr)
     #plt.plot(x, w0)
     #plt.plot(x_local, d1, "b--")
     #plt.plot(x_local, d2, "r--")
     #N1_arr = [N1(i/points) for i in range (0,points+1)]
     #N2_arr = [N2(i/points) for i in range (0,points+1)]
     #print(f"c2 = {c2}")
-    #plt.plot(x, N2_arr)
+    #plt.plot(x_local, N2_arr)
     #plt.plot(x, N1_arr)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    print("Global   --- %s seconds ---" % (time.time() - start_time))
     plt.show()
